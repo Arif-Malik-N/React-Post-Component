41303c1e1d6f80a3f31fc01aa1611fb27215ca6f
Arif-Malik-N <arif.malik121215@gmail.com>
2020-12-04 05:11:09 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import'./App.css'
+
+function App({imgUrl,name,userPic,postText}) {
+  return (
+    <div className = "postContainer">
+      <div className = "head">
+      
+        <div  > 
+          </div>
+        <br/>
+        <div className ="uname" >
+           <img className = "propic" src = {userPic}></img>
+        
+        </div>
+      <div className ="uname nm" >
+  <strong><span>{name}</span></strong>  <br></br>
+        <span> <i class="fas fa-users userIcon"></i>12 Dec 5:51 PM</span>
+        </div>
+        <div className ="moreBtn" > <i class="fas fa-ellipsis-h" ></i></div>
+      </div>
+  <div className = "postTxt">{postText}</div>
+      <div>
+     <img width = "400px" height = "300px" src = {imgUrl}></img>
+      </div>
+      <div className= "lines"></div>
+      <div className = "likeCommShare">
+       <span className = "like"> <i  class="far fa-thumbs-up">  </i> <span className ="commanTxt">Like</span></span> 
+       <span  className = "comment" ><i class="far fa-comment-alt"></i><span className ="commanTxt">Comment</span></span>
+       <span className = "share"><i  class="fas fa-share"> </i><span className ="commanTxt"></span>Share</span>
+      </div>
+      <div className= "lines"></div>
+    </div>
+  )
+}
+
+export default App
+
---
+++ src/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App name = "Bilal Khan" 
+       userPic = "https://scontent.fkhi5-1.fna.fbcdn.net/v/t1.0-9/127242689_1689834651178220_443739335900519811_o.jpg?_nc_cat=109&ccb=2&_nc_sid=09cbfe&_nc_eui2=AeE69XyS6Nf5nUEz61Zdp1Bg9TReJldDh5H1NF4mV0OHkYQXfGMa5Cw_Jcl02X8jJdRpy5HXxJ6NWi1Hxy7bqTsU&_nc_ohc=fPj0T_g-SvkAX-11END&_nc_ht=scontent.fkhi5-1.fna&oh=84632f82b36ece8ad9b148ef6f646070&oe=5FEBE3DB"
+       postText = "Hi there"
+       imgUrl = "https://www.voicesofyouth.org/sites/voy/files/images/2019-11/theimportanceofethicaltravel.jpg" 
+        />
+
+<App name = "Ali Jan Malik" 
+       userPic = "https://scontent.fkhi5-1.fna.fbcdn.net/v/t1.0-9/84671468_482258739343753_1565019502079377408_o.jpg?_nc_cat=110&ccb=2&_nc_sid=174925&_nc_eui2=AeG7S8mUOlR8JQdEsv2ytHdeQBMGJranDCtAEwYmtqcMKxi2drNRZdFbScoJmvO5vJ6y0M9SuSFG3gJWJQCeuEtg&_nc_ohc=Dg7YMrUdpy8AX9s1xHE&_nc_ht=scontent.fkhi5-1.fna&oh=27ac1e00d3d343071e38e01001ea7d59&oe=5FED4086"
+       postText = "2nd December happy independent day :UAE"
+       imgUrl = "https://scontent.fkhi5-1.fna.fbcdn.net/v/t1.0-9/128806842_10157883201622864_7301359486372701066_o.jpg?_nc_cat=106&ccb=2&_nc_sid=730e14&_nc_eui2=AeEL9yi43ag1QGp70smZsvKP3TfDl743g8DdN8OXvjeDwIM3y36geuqvcsz5h0iQ5nyqQB1qX7fzuxUK2_IRA8_o&_nc_ohc=hyUK179Ln-QAX-Aon3s&_nc_ht=scontent.fkhi5-1.fna&oh=f2a2ce361be928c2fd549704a7123a97&oe=5FEEA1C0" 
+        />
+
+<App name = "Adnan Malik" 
+       userPic = "https://scontent.fkhi5-1.fna.fbcdn.net/v/t1.0-0/p180x540/128566469_2743292525888525_3101261291899887664_o.jpg?_nc_cat=110&ccb=2&_nc_sid=e3f864&_nc_eui2=AeEA3UYndneQxbLRO8lJ3fvUax3wKodrZ5NrHfAqh2tnk_muF9rbWfbKrILi_ywRh5J_KKUKR4CAbxdWqzY-YMOo&_nc_ohc=cUb9er11m4YAX_fYB_J&_nc_ht=scontent.fkhi5-1.fna&tp=6&oh=7d2f9c3ae7064eadbdb91c1ff6003885&oe=5FF03754"
+       postText = "The first scheduled commercial flight operated by flydubai lands at Tel Aviv Ben Gurion International."
+       imgUrl = "https://scontent.fkhi5-1.fna.fbcdn.net/v/t1.0-9/127189278_10157869772382864_8407403575540685602_o.jpg?_nc_cat=101&ccb=2&_nc_sid=730e14&_nc_eui2=AeEuc52xFRD_jHYNNVwnLZBiheXnx4EAGH6F5efHgQAYfroY6SL_AwuVMyjaCz3UTRSkugNWbLI9iVag5qZvrEAw&_nc_ohc=39CBSQpTq-cAX_6Gglz&_nc_ht=scontent.fkhi5-1.fna&oh=e87f0e45958ac0afe9bb0bea4bab9a7a&oe=5FEE483B"
+        />
+
+
+
+
+
+    
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
+
+reportWebVitals();
